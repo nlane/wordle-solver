ef8fd509c9ac728ed934b5516e12dec9491f6b68
Natalie Lane <nlane@udel.edu>
2022-02-23 13:17:51 -0500

    updates letter and word scoring

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 )
 
 var words [5757]string
-var letterScores = make(map[string]int)
+var letterScores = make(map[string]*[5]int)
 var wordScores = make(map[string]int)
 var wordMap = make(map[string]*[5][]string)
 var currGuess string
@@ -18,7 +18,7 @@ var answer string
 
 func main() {
 
-    flag.StringVar(&currGuess, "s", "thing", "Specify starting word. Default is 'thing'.")
+    flag.StringVar(&currGuess, "s", "cares", "Specify starting word. Default is 'cares'.")
     flag.StringVar(&answer, "a", "", "Specify answer. Default mode runs without answer.")
 
     flag.Parse()
@@ -29,8 +29,10 @@ func main() {
     currWordBank := words[:];
     guessCount := 1
 
+
     if answer != "" {
         for {
+            currGuess = pickBestWord(currWordBank)
             fmt.Println("My guess is: " + currGuess)
             res := checkGuess(currGuess, answer)
             if res == "YYYYY" {
@@ -102,13 +104,14 @@ func processWordFile() {
     scanner := bufio.NewScanner(file)
     for i:=0; scanner.Scan(); i++ {
         words[i] = scanner.Text();
-        for _, c := range words[i] {
+        for i, c := range words[i] {
             letter := string(c)
-            value, mapContainsLetter := letterScores[letter]
+            _, mapContainsLetter := letterScores[letter]
             if mapContainsLetter {
-                letterScores[letter] = value + 1
+                letterScores[letter][i] += 1
             } else {
-                letterScores[letter] = 1
+                letterScores[letter] = &[5]int{0, 0, 0, 0, 0}
+                letterScores[letter][i] = 1
             }
         }
     }
@@ -119,17 +122,17 @@ func processWordFile() {
 }
 
 // Function to calculate the "score" of a given word
-// Takes into account the popularity of letters and 
-// prefers words with non-repeating letters
+// based on the frequency and position of the letters 
+// and on the variety of letters in the word
 func calcScore(word string) (int) {
     score := 0
-    for _, c := range word {
+    for i, c := range word {
         letter := string(c)
-        score += letterScores[letter]
-        // n1 := strings.Count(word, letter)
-        // if n1 == 1 {
-        //     score += 750
-        // }
+        score += letterScores[letter][i]
+        n1 := strings.Count(word, letter)
+        if n1 == 1 {
+            score += 200
+        }
     }
     return score
 }
